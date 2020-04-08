c7cda9465dab7a8a5fd6e57ed3d665d7a9550111
Kasper Kalfas <aiflop.blog@gmail.com>
2020-04-08 09:42:41 +0200

    post code validation

--- MDMProject/Content/scripts/MapMDM.js
+++ MDMProject/Content/scripts/MapMDM.js
@@ -1,5 +1,7 @@
 ﻿
-var mymap = L.map('mapID').setView([51.505, -0.09], 13);
+window.onload = loadMapSuppliers;
+
+var mymap = L.map('mapID').setView([51.643078, 19.609658], 7);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -21,10 +23,34 @@ var iconsPrinter = L.icon({
     popupAnchor: [-3, -76]
 });
 
-L.marker([51.5, -0.09], { icon: iconsMask }).addTo(mymap);
-L.marker([51.51, -0.10], { icon: iconsPrinter }).addTo(mymap);
+L.marker([51.643078, 19.609658], { icon: iconsMask }).addTo(mymap);
+L.marker([51.643, 20.60], { icon: iconsPrinter }).addTo(mymap);
 
 var popup = L.popup();
+
+function loadMapSuppliers() {
+    // todo get all supliers
+
+}
+
+function SPCButtonValidation(code) {
+    if (code.match(/\d{2}-\d{3}/)) {
+        return true;
+    }
+    document.getElementById("search_validation").style.display = "block";
+    return false;
+}
+
+function onSPCButtonClick(e) {
+    codeVal = document.getElementById("search").value;
+    document.getElementById("search_validation").style.display = "none";
+    if (SPCButtonValidation(codeVal)) {
+        // todo ask api geolocalization 
+        // change zoom map
+        // hide supliers
+        // hide markes
+    }
+}
 
 function onMapClick(e) {
     popup
@@ -32,5 +58,7 @@ function onMapClick(e) {
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(mymap);
 }
+
+
 
 mymap.on('click', onMapClick);
