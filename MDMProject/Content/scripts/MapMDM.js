637e0ec9a657ae92bec2998112520c855e597cac
Kasper Kalfas <aiflop.blog@gmail.com>
2020-04-08 13:06:29 +0200

    load suppliers

--- MDMProject/Content/scripts/MapMDM.js
+++ MDMProject/Content/scripts/MapMDM.js
@@ -1,6 +1,8 @@
 ﻿
 window.onload = loadMapSuppliers;
 
+var suppliersList = [];
+
 var mymap = L.map('mapID').setView([51.643078, 19.609658], 7);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -28,10 +30,7 @@ L.marker([51.643, 20.60], { icon: iconsPrinter }).addTo(mymap);
 
 var popup = L.popup();
 
-function loadMapSuppliers() {
-    // todo get all supliers
 
-}
 
 function SPCButtonValidation(code) {
     if (code.match(/\d{2}-\d{3}/)) {
@@ -49,6 +48,7 @@ function onSPCButtonClick(e) {
         // change zoom map
         // hide supliers
         // hide markes
+
     }
 }
 
@@ -59,6 +59,108 @@ function onMapClick(e) {
         .openOn(mymap);
 }
 
-
-
 mymap.on('click', onMapClick);
+
+var ListElement = {
+
+    htmlEl : "<li class=\"list__item\" id=\"#Id\"> <div role = \"button\" class= \"entry #Type\" >" +
+             "<div class=\"entry__wrapper p-1\"><div class=\"entry__icon\">"+
+            "<img src=#Src alt=#Alt></div><h3 class=\"entry__text pl-1\">#Name</h3></div>" +
+                    "<address class=\"entry__address p-1\">#Adress</address>"+
+                        "<div class=\"entry__media p-1\">"+
+                            "tel: <a href=\"tel:#Phone\" title=\"Zadzwoń: #Phone\">#Phone</a>"+
+                            "<br> e-mail:<a href=\"mailto:#Email\"title=\"Wyślij e-mail: #Email\">#Email</a>"+
+                            "</div></div></li>",
+
+    createLi: function (item) {
+            var tile = this.htmlEl;
+            tile = tile.replace('#Id', 'supplier-' + item.Id);
+            tile = tile.replace("#Name", item.Name);
+            var adress = item.Address.City + " " + item.Address.PostalCode + ", <br/>" + item.Address.StreetName + " " + item.Address.HouseNumber;
+            if (item.Address.FlatNumber !== null) {
+                adress = adress + " " + item.Address.FlatNumber;
+            }
+            tile = tile.replace("#Adress", adress);
+
+            tile = tile.replace(/#Phone/gi, item.PhoneNumber);
+            tile = tile.replace(/#Email/gi, item.Email);
+        if (item.OfferedHelp !== null && item.OfferedEquipment !== null) {
+            tile = tile.replace('<img src=#Src alt=#Alt>', '<img src=\"/Content/images/mask.png\" alt=\"Drukuję w 3D\"><img src=\"/Content/images/3d.png\" alt=\"Oferuję Maskę"\>')
+
+            tile = tile.replace(/#Type/gi, "multiple");
+
+            }
+            else if(item.OfferedHelp !== null) {
+            // 3d
+                tile = tile.replace(/#Src/gi, "/Content/images/3d.png");
+                tile = tile.replace(/#Alt/gi, "Drukuję w 3D");
+                tile = tile.replace(/#Type/gi, "print");
+            }
+            else if (item.OfferedEquipment !== null) {
+            //mask
+                tile = tile.replace(/#Src/gi, "/Content/images/mask.png");
+                tile = tile.replace(/#Alt/gi, "Oferuję Maskę");
+                tile = tile.replace(/#Type/gi, "mask");
+        }
+        var liElement = document.createElement('li');
+        liElement.innerHTML = tile;
+        return liElement;
+
+        }
+    }
+
+function loadMapSuppliers() {
+    $.ajax({
+        type: "GET",
+        url: '/api/suppliers',
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (value) {
+            suppliersList = value;
+            console.log(value);
+            var el = document.getElementById('showSuppliers');
+            for (var i = 0; i < suppliersList.length; i++) {
+                if (suppliersList[i].OfferedHelp !== null || suppliersList[i].OfferedEquipment !== null)
+                    el.appendChild(ListElement.createLi(suppliersList[i]));
+            }
+
+        },
+        error: function () {
+            alert("Error while inserting data");
+        }
+    });
+
+}
+//#id X
+//#Src
+//#Alt
+
+//#Name X
+
+//#Adress X
+
+//#Phone
+//#Email
+
+///api/suppliers - zwraca wszystkich dostawcow(masek lub druku)
+//api/suppliers/17 - zwraca tylko gościa o id 17
+
+//Id: "13"
+//Name: "Krzysztof Kowalkiewicz"
+//Email: "krzysztof@kowalkiewicz.pl"
+//PhoneNumber: "+48 77 12 50 321"
+//Address:
+//City: "Opole"
+//StreetName: "Krakowska"
+//HouseNumber: "44"
+//FlatNumber: null
+//PostalCode: "45-075"
+//Latitude: null
+//Longitude: null
+//__proto__: Object
+//OfferedEquipment: Array(1)
+//0: { Name: "Maska", Amount: 1, Comment: null }
+//length: 1
+//__proto__: Array(0)
+//OfferedHelp: null
+//AdditionalComment: "Tylko po 17:00!"
