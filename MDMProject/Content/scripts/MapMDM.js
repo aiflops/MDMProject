3012ab15063115329eb6de125e0f60400f39ecbe
Kasper Kalfas <aiflop.blog@gmail.com>
2020-04-08 08:49:56 +0200

    map styles

--- MDMProject/Content/scripts/MapMDM.js
+++ MDMProject/Content/scripts/MapMDM.js
@@ -1,11 +1,36 @@
-﻿var mymap = L.map('mapid').setView([51.505, 15.50], 10);
+﻿
+var mymap = L.map('mapID').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox/streets-v11',
-	tileSize: 512,
-	zoomOffset: -1
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
 }).addTo(mymap);
+
+var iconsMask = L.icon({
+    iconUrl: '/Content/images/mask.png',
+    iconSize: [40, 40],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+var iconsPrinter = L.icon({
+    iconUrl: '/Content/images/3d.png',
+    iconSize: [40, 40],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+
+L.marker([51.5, -0.09], { icon: iconsMask }).addTo(mymap);
+L.marker([51.51, -0.10], { icon: iconsPrinter }).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
