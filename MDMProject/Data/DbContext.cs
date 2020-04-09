ca828d3c017eb04cd03831ef38c8e6c7c758fd2e
Krzysztof Kuhn <krzysztof.kuhn@prosoftlab.pl>
2020-04-09 12:01:20 +0200

    Reorganized Identity - split model and DbContext

---
+++ MDMProject/Data/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using MDMProject.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MDMProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("ApplicationDbConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
