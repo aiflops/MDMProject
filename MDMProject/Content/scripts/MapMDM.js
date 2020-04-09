3c7438bf7034c527490caba47a9ae6792292d30f
Kasper Kalfas <aiflop.blog@gmail.com>
2020-04-09 11:28:17 +0200

    clusters, click item

--- MDMProject/Content/scripts/MapMDM.js
+++ MDMProject/Content/scripts/MapMDM.js
@@ -9,19 +9,23 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1
 }).addTo(mymap);
+var popup = L.popup();
+
 function customParseFloat(lat, lon) {
     return [parseFloat(lat), parseFloat(lon)];
 }
-var MapClass = {
+var MapMDM = {
+    onInitFlag: true,
     setView: function (lat, lon, zoom) {
         mymap.setView(customParseFloat(lat,lon), zoom);
     },
     
     moveMap: function (e) {
         ListElement.clearAllLi();
+        //MarkersGroupMDM.clearAllMarkers();
         var bounds = this.getBounds();
-        MapClass.checkIfSupplierOnMap(bounds);
-        //_southWest _northEast
+        MapMDM.checkIfSupplierOnMap(bounds);
+        //MapMDM.addCluster(MarkersGroupMDM.cluster);
     },
     checkIfSupplierOnMap: function (bounds) {
         for (var i = 0; i < suppliersList.length; i++) {
@@ -32,10 +36,13 @@ var MapClass = {
             }
                 
         }
+    },
+    addCluster: function (cluster) {
+        mymap.addLayer(cluster);
     }
 
 };
-mymap.on('moveend', MapClass.moveMap);
+mymap.on('moveend', MapMDM.moveMap);
 function SPCButtonValidation(code) {
     if (code.match(/\d{2}-\d{3}/)) {
         return true;
@@ -56,7 +63,7 @@ function onSPCButtonClick(e) {
             success: function (value) {
                 console.log(value);
                 var firstResult = value[0];
-                MapClass.setView(firstResult.lat, firstResult.lon, 12);
+                MapMDM.setView(firstResult.lat, firstResult.lon, 12);
             },
             error: function () {
                 alert("Error while inserting data");
@@ -65,19 +72,19 @@ function onSPCButtonClick(e) {
     }
 };
 var ListElement = {
-    liListID:[],
-    htmlEl : "<li class=\"list__item\" id=\"#Id\"> <div role = \"button\" class= \"entry #Type\" >" +
+    liListID: [],
+    activeElement: '',
+    htmlEl: "<div role = \"button\" class= \"entry #Type\" >" +
              "<div class=\"entry__wrapper p-1\"><div class=\"entry__icon\">"+
             "<img src=#Src alt=#Alt></div><h3 class=\"entry__text pl-1\">#Name</h3></div>" +
                     "<address class=\"entry__address p-1\">#Adress</address>"+
                         "<div class=\"entry__media p-1\">"+
                             "tel: <a href=\"tel:#Phone\" title=\"Zadzwoń: #Phone\">#Phone</a>"+
                             "<br><a href=\"mailto:#Email\"title=\"Wyślij e-mail: #Email\">#Email</a>"+
-                            "</div></div></li>",
+                            "</div></div>",
 
     createLi: function (item) {
         var tile = this.htmlEl;
-        this.liListID.push('supplier-' + item.Id);
         tile = tile.replace('#Id', 'supplier-' + item.Id);
         var adress = item.Address.City + ", " + item.Address.StreetName + " " + item.Address.HouseNumber;
         if (item.Address.FlatNumber !== null) {
@@ -87,27 +94,41 @@ var ListElement = {
         if (item.OfferedHelp !== null && item.OfferedEquipment !== null) {
             tile = tile.replace('<img src=#Src alt=#Alt>', '<img src=\"/Content/images/mask.png\" alt=\"Drukuję w 3D\"><img src=\"/Content/images/3d.png\" alt=\"Oferuję Maskę"\>')
             tile = tile.replace(/#Type/gi, "multiple");
+            if (MapMDM.onInitFlag)
+                MarkerMDM.setMarker(item.Address.Latitude, item.Address.Longitude, MarkerMDM.iconsPrinter, item.Id);
         }
         else if(item.OfferedHelp !== null) {
         tile = tile.replace(/#Src/gi, "/Content/images/3d.png").replace(/#Alt/gi, "Drukuję w 3D").replace(/#Type/gi, "print");
-        MarkerMDM.setMarker(item.Address.Latitude, item.Address.Longitude, MarkerMDM.iconsPrinter, item.Id);
+            if (MapMDM.onInitFlag)
+            MarkerMDM.setMarker(item.Address.Latitude, item.Address.Longitude, MarkerMDM.iconsPrinter, item.Id);
         }
         else if (item.OfferedEquipment !== null) {
-        tile = tile.replace(/#Src/gi, "/Content/images/mask.png").replace(/#Alt/gi, "Oferuję Maskę").replace(/#Type/gi, "mask");
-        MarkerMDM.setMarker(item.Address.Latitude, item.Address.Longitude, MarkerMDM.iconsMask, item.Id);
+            tile = tile.replace(/#Src/gi, "/Content/images/mask.png").replace(/#Alt/gi, "Oferuję Maskę").replace(/#Type/gi, "mask");
+            if (MapMDM.onInitFlag)
+                MarkerMDM.setMarker(item.Address.Latitude, item.Address.Longitude, MarkerMDM.iconsMask, item.Id);
         }
         var liElement = document.createElement('li');
+        liElement.classList.add("list__item");
+        liElement.id = 'supplier-' + item.Id;
+        if (ListElement.activeElement === liElement.id) {
+            liElement.classList.add("active__item");
+        }
+        liElement.dataset.latlng = item.Address.Latitude + '-' + item.Address.Longitude;
+        liElement.addEventListener("click", ListElement.clickLi);
         liElement.innerHTML = tile;
         return liElement;
     },
     clearAllLi: function () {
         suppliersListElHtml.innerHTML = "";
+        this.liListID = [];
     },
     createMoreLi: function (itemsList) {
+        MarkersGroupMDM.clearAllMarkers();
         for (var i = 0; i < itemsList.length; i++) {
             if (itemsList[i].OfferedHelp !== null || itemsList[i].OfferedEquipment !== null)
                 suppliersListElHtml.appendChild(this.createLi(itemsList[i]));
         }
+        MapMDM.addCluster(MarkersGroupMDM.cluster);
     },
     createOneLi: function (item) {
         suppliersListElHtml.appendChild(this.createLi(item));
@@ -118,30 +139,54 @@ var ListElement = {
     showLiElement: function (itemId) {
         document.getElementById(itemId).style.display = "list-item";
     },
+    clickLi: function (e) {
+        if (this.dataset.latlng!=="null-null")
+            MapMDM.setView(this.dataset.latlng.split('-')[0], this.dataset.latlng.split('-')[1],20);
+    }
 }
+var MarkersGroupMDM = {
+    cluster : L.markerClusterGroup({
+        chunkedLoading: true,
+        spiderfyOnMaxZoom: false,
+        disableClusteringAtZoom: 22,
+        showCoverageOnHover: false,
+        removeOutsideVisibleBounds: true,
+        polygonOptions: null,
+    }),
+    addMarker: function (marker) {
+        MarkersGroupMDM.cluster.addLayer(marker);
+    },
+    clearAllMarkers: function () {
+        MarkersGroupMDM.cluster.clearLayers();
+    }
+};
+
 var MarkerMDM = {
     markerList:[],
     iconsMask: L.icon({
         iconUrl: '/Content/images/mask.png',
-        iconSize: [40, 40],
+        iconSize: [50, 50],
         iconAnchor: [22, 94],
         popupAnchor: [-3, -76]
     }),
     iconsPrinter : L.icon({
         iconUrl: '/Content/images/3d.png',
-        iconSize: [40, 40],
+        iconSize: [50, 50],
         iconAnchor: [22, 94],
         popupAnchor: [-3, -76]
     }),
     onClick: function (e) {
-        MapClass.setView(this.getLatLng().lat, this.getLatLng().lng, 14);
+        var key = customParseFloat(this.getLatLng().lat, this.getLatLng().lng).join("-")
+        //ListElement.activeElement = MarkerMDM.markerList[key];
     },
     setMarker: function (lat, lon, icon, htmlId) {
     if (lat !== null && lon !== null) {
         var latD = parseFloat(lat);
         var lonD = parseFloat(lon);
-        L.marker([latD, lonD], { icon: icon }).addTo(mymap).on('click', this.onClick);
+        //L.marker([latD, lonD], { icon: icon }).addTo(mymap).on('click', this.onClick);
         this.markerList[latD + "-" + lonD] = 'supplier-' + htmlId;
+        var marker = L.marker([latD, lonD], { icon: icon }).on('click', MarkerMDM.onClick);
+        MarkersGroupMDM.addMarker(marker);
     }
     },
 };
@@ -156,6 +201,7 @@ function loadMapSuppliers() {
             suppliersList = value;
             console.log(value);
             ListElement.createMoreLi(suppliersList);
+            MapMDM.onInitFlag = false;
         },
         error: function () {
             alert("Error while inserting data");
